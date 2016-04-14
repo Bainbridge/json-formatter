418939dc731b82c194a662deead1b9b259a034d1
Cortlan Bainbridge <bainbric@uni.edu>
2016-04-14 09:52:59 -0500

    Added some extras to this, I usually end up with thousands of -formatted.json files so here is an argument that will clean the directory of all "*-formatted.json"s!  This uses the glob module and the os module to get the directory you are currently working in

--- format.py
+++ format.py
@@ -8,16 +8,21 @@ import json
 import sys
 import argparse
 import os
+import glob
 
 #Arguments
 parser = argparse.ArgumentParser(description="Make a JSON human readable")
 parser.add_argument('-f', action="store",required=False,help="Specifiy a file name")
+parser.add_argument('-c', action="store_true",required=False,help="Cleans the directory of all formatted JSONs")
 args = parser.parse_args()
 
 TAB = "\t"
 NL = "\n"
 
 def main():
+	if args.c:
+		cleanDirectory()
+		return
 	if args.f:
 		verified = checkFileName(args.f)
 		if verified:
@@ -76,5 +81,17 @@ def checkFileName(fileName):
 		else:
 			print("File doesn't exist")
 	return False
+
+def cleanDirectory():
+	counter = 0
+	files = glob.glob(os.getcwd()+"\*")
+	print("Cleaning the directory of past update scripts...")
+	for specificFile in files:
+		specificFileSplit = specificFile.split("/")
+		if "formatted.json" in specificFileSplit[-1]:
+			os.remove(specificFile)
+			print("Removed "+specificFile)
+			counter += 1
+	print("    Removed "+str(counter)+" formatted jsons.")
 
 main()
